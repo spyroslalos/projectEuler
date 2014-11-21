e797fd5315151b815002994edd9d48530c53facc
KizuRos <spyroslal@gmail.com>
2014-11-21 23:07:57 +0200

    problem6 fine imple

--- problem6.py
+++ problem6.py
@@ -2,9 +2,9 @@
 """
 #=============================================================================#
 
-FILE            : problem4.py
+FILE            : problem6.py
 
-DESCRIPTION     : Project Euler--Problem3 solution/implementation
+DESCRIPTION     : Project Euler--Problem6 solution/implementation
 		          in Python
 		        :
 
@@ -24,12 +24,17 @@ import sys
 import math
 
 
-n = 10
+n = 100
 
 # Sum of squares first 10
 sqrsum = ( n*(n+1)*(2*n + 1) )/6
 print sqrsum
 
 # Square of sums
-sumsqr = math.pow(( n*(1+10) )/2, 2)
+sumsqr = math.pow(( n*(1+100) )/2, 2)
 print sumsqr
+
+print "{0} is : {1}".format(sumsqr, sqrsum)
+
+print  "Difference is : {0}".format(sumsqr - sqrsum)
+
