eabe50d01d09810e157d7f351e25d82ea87c325f
KizuRos <spyroslal@gmail.com>
2014-11-24 20:09:29 +0200

    prime function in my api

---
+++ myMathAPI.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python2.7
+"""
+#=============================================================================#
+
+FILE            : myMathAPI.py
+
+DESCRIPTION     : My personal math module in python
+
+REQUIREMENTS    : None
+BUGS            : Must be somewhere hiding :P.
+NOTES           : 
+AUTHOR(s)       : Spyros Lalos (spyroslal@gmail.com)
+CREATED         : Nov 24 #:#:# CEST 2014
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the Cern.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+#=============================================================================#
+"""
+import math
+
+def is_prime (n):
+    for i in range (2,int(math.sqrt(num)+1)):
+        if ( n%i ) == 0:
+            return False
+    return True
