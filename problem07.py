dad8ace832f7b386d8a383ae92528d3b89e55e98
KizuRos <spyroslal@gmail.com>
2014-11-25 16:14:40 +0200

    initial commit problem07

---
+++ problem07.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python2.7
+"""
+#=============================================================================#
+
+FILE            : problem07.py
+
+DESCRIPTION     : Project Euler--Problem7 solution/implementation in Python
+                  Find the 10001 prime number
+REQUIREMENTS    : None
+BUGS            : Must be somewhere hiding :P.
+NOTES           : 
+AUTHOR(s)       : Spyros Lalos (spyroslal@gmail.com)
+CREATED         : Nov 24 #:#:# CEST 2014
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the Cern.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+#=============================================================================#
+"""
