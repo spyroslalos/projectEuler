08a777ab3d91811a173a7226fa255838e1f84f51
KizuRos <spyroslal@gmail.com>
2014-11-27 20:14:36 +0200

    new empty problem04

---
+++ problem04.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python2.7
+"""
+#=============================================================================#
+
+FILE            : problem04.py
+
+DESCRIPTION     : Project Euler--Problem04 solution/implementation in Python
+                  Find the largest possible palindronme
+REQUIREMENTS    : None
+BUGS            : Must be somewhere hiding :P.
+NOTES           : 
+AUTHOR(s)       : Spyros Lalos (spyroslal@gmail.com)
+CREATED         : Nov 27 #:#:# CEST 2014
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the Cern.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+#=============================================================================#
+"""
+
