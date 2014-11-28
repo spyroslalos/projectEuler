52b3a093b5bf344bd2a4aa5f8742527598d32efe
KizuRos <spyroslal@gmail.com>
2014-11-28 22:50:31 +0200

    problem25 working

---
+++ problem25.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python2.7
+"""
+#=============================================================================#
+
+FILE            : problem25.py
+
+DESCRIPTION     : Project Euler--Problem25 solution/implementation
+		          in Python
+
+REQUIREMENTS    : None
+BUGS            : Must be somewhere hiding :P.
+NOTES           : 
+AUTHOR(s)       : Spyros Lalos (spyroslal@gmail.com)
+CREATED         : Nov 06 23:04:12 CEST 2014
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the Cern.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+#=============================================================================#
+"""
+import math
+import sys
+
+
+# define start integers
+a = 1
+b = 2 
+term = 3
+flist = []
+flist.append (1)
+flist.append (2)
+c = a + b
+sum = 2
+
+while True :
+    c = a + b
+    term += 1
+    if (int(math.log10(c))+1) == 1000:
+        break
+    a = b
+    b = c
+
+print "First fibo with 1000 digits = %d" % (term)
