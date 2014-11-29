9c8e6778e096c2c9517b45542efc5db41635a49b
KizuRos <spyroslal@gmail.com>
2014-11-29 17:33:18 +0200

    empty temp problem08

---
+++ problem08.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/python2.7
+"""
+#=============================================================================#
+FILE            : problem08.py
+
+DESCRIPTION     : Project Euler--Problem08 solution/implementation in Python
+                  Find 13th adjacent digits max program. 
+REQUIREMENTS    : None
+BUGS            : Must be somewhere hiding :P.
+NOTES           : 
+AUTHOR(s)       : Spyros Lalos (spyroslal@gmail.com)
+CREATED         : Nov 29 #:#:# CEST 2014
+#=============================================================================#
+"""
+
+
