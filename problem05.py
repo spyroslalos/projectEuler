80d284cb678d3cc4ebd64d1a55eab03489ca3390
KizuRos <spyroslal@gmail.com>
2014-11-30 20:50:48 +0200

    initial empty file for problem05

---
+++ problem05.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python2.7
+"""
+#=============================================================================#
+
+FILE            : problem05.py
+
+DESCRIPTION     : Project Euler--Problem7 solution/implementation in Python
+                  Find smallest num evenly divised by [1, ..., 20]
+REQUIREMENTS    : None
+BUGS            : Must be somewhere hiding :P.
+NOTES           : 
+AUTHOR(s)       : Spyros Lalos (spyroslal@gmail.com)
+CREATED         : Nov 30 #:#:# CEST 2014
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the Cern.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+#=============================================================================#
+"""
