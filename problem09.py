008950d9dbf4e59a85534723cfab00d375bc1d7e
KizuRos <spyroslal@gmail.com>
2014-12-01 17:58:48 +0200

    empty temp prob09

---
+++ problem09.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python2.7
+"""
+#=============================================================================#
+
+FILE            : problem09.py
+
+DESCRIPTION     : Project Euler--Problem09 solution/implementation in Python
+                  Pythagorean Triples
+REQUIREMENTS    : None
+BUGS            : Must be somewhere hiding :P.
+NOTES           : 
+AUTHOR(s)       : Spyros Lalos (spyroslal@gmail.com)
+CREATED         : Dec 01 #:#:# CEST 2014
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the Cern.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+#=============================================================================#
+"""
