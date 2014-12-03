cdfa261b47ebede78ea4c0cc76d3a2875ad9670c
KizuRos <spyroslal@gmail.com>
2014-12-03 18:59:56 +0200

    problem13 solution

--- problem13.py
+++ problem13.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python2.7
 """
 #=============================================================================#
-
 FILE            : problem13.py
 
 DESCRIPTION     : Project Euler--Problem13 solution/implementation in Python
                   Find the 10-first digits of large sum
 
-REQUIREMENTS    : None
-BUGS            : Must be somewhere hiding :P.
-NOTES           : 
-AUTHOR(s)       : Spyros Lalos (spyroslal@gmail.com)
-CREATED         : Nov 24 #:#:# CEST 2014
-
-    This program is free software: you can redistribute it and/or modify
-    it freely.
-
 #=============================================================================#
 """
 
-
+input = open ("input13")
+print sum([int(line) for line in input])
+input.close()
