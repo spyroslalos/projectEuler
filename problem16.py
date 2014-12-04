453f8e3257854ea9c1a18feacfd0bdce87a057f8
KizuRos <spyroslal@gmail.com>
2014-12-04 18:07:46 +0200

    prob16 solution

---
+++ problem16.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2.7
+"""
+#=============================================================================#
+FILE            : problem20.py
+DESCRIPTION     : Project Euler--Problem16 solution/implementation in Python
+                  Sum of digits
+AUTHOR(s)       : Spyros Lalos (spyroslal@gmail.com)
+#=============================================================================#
+"""
+from myMathAPI import dumb_sum_digits
+
+# Print sum of digits 
+print dumb_sum_digits(2**1000)
