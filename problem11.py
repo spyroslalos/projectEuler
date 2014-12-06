083670ae79ff57bae5f167f67425916fcb3f5198
KizuRos <spyroslal@gmail.com>
2014-12-06 17:08:40 +0200

    add empty prob11

---
+++ problem11.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2.7
+"""
+#=============================================================================#
+FILE            : problem11.py
+
+DESCRIPTION     : Project Euler--Problem11 solution/implementation in Python
+                  Max product on Grid
+
+REQUIREMENTS    : None
+BUGS            : Must be somewhere hiding :P.
+NOTES           : 
+AUTHOR(s)       : Spyros Lalos (spyroslal@gmail.com)
+CREATED         : Dec 6 #:#:# CEST 2014
+
+    This program is free software: you can redistribute it and/or modify
+    it freely.
+#=============================================================================#
+"""
